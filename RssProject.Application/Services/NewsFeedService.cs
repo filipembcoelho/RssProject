c4b74e13e23d97535a7663a1f14b660920a43b86
Filipe Coelho <filipemcoelho@gmail.com>
2021-10-29 20:12:55 +0100

    First implementation of NewsFeedService

--- RssProject.Application/Services/NewsFeedService.cs
+++ RssProject.Application/Services/NewsFeedService.cs
@@ -1,20 +1,34 @@
-﻿using RssProject.Application.Contracts;
+﻿using AutoMapper;
+using RssProject.Application.Contracts;
 using RssProject.Application.Dtos;
+using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace RssProject.Application.Services
 {
     public class NewsFeedService : INewsFeedService
     {
+        private readonly IMapper _mapper;
 
-        public NewsFeedService()
+        public NewsFeedService(IMapper mapper)
         {
+            this._mapper = mapper;
         }
 
-
-        public Task<NewsFeedDto> GetRssNewsFeed(string url)
+        public async Task<NewsFeedDto> GetRssNewsFeed(string url)
         {
-            throw new System.NotImplementedException();
+            var feed = await GetFeed(url);
+            var newsFeed = _mapper.Map<NewsFeedDto>(feed);
+            return newsFeed;
+        }
+
+        private static async Task<SyndicationFeed> GetFeed(string url = null)
+        {
+            url ??= "https://feeds.services.tv2.dk/api/feeds/nyheder/rss";
+            using var reader = XmlReader.Create(url);
+            var feed = await Task.Run(() => SyndicationFeed.Load(reader));
+            return feed;
         }
     }
 }
