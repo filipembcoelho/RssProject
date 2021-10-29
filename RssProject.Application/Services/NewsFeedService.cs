a4c0cf198e4c6b730571aca947e9cdf45db29851
Filipe Coelho <filipemcoelho@gmail.com>
2021-10-29 20:51:11 +0100

    Check for authors today

--- RssProject.Application/Services/NewsFeedService.cs
+++ RssProject.Application/Services/NewsFeedService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using RssProject.Application.Contracts;
 using RssProject.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
 using System.Xml;
@@ -19,7 +22,16 @@ namespace RssProject.Application.Services
         public async Task<NewsFeedDto> GetRssNewsFeed(string url)
         {
             var feed = await GetFeed(url);
+
+            var count = feed.Items
+                .Where(x => x.PublishDate.UtcDateTime >= DateTime.UtcNow.AddDays(-1))
+                .Select(x => x.Authors)
+                .Select(x => x.Count)
+                .Sum();
+
             var newsFeed = _mapper.Map<NewsFeedDto>(feed);
+
+            newsFeed.AuthorsToday = count;
             return newsFeed;
         }
 
