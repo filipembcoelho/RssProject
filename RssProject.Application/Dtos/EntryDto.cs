3b92e1da5ad74040be5ab6b76b13e61c2e350607
Filipe Coelho <filipemcoelho@gmail.com>
2021-10-29 20:07:02 +0100

    Created DTOs

---
+++ RssProject.Application/Dtos/EntryDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RssProject.Application.Dtos
+{
+    public class EntryDto
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+        public IEnumerable<string> Authors { get; set; }
+    }
+}
---
+++ RssProject.Application/Dtos/NewsFeedDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RssProject.Application.Dtos
+{
+    public class NewsFeedDto
+    {
+        public string Title { get; set; }
+        public int AuthorsToday { get; set; }
+        public string Desc { get; set; }
+        public string ImgUrl { get; set; }
+        public IEnumerable<EntryDto> Entries { get; set; }
+    }
+}
